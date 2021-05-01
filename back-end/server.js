a2d64660ee5484051e908c8f13808505c056b01b
duc410 <ducnguyennhu410@gmail.com>
2021-05-01 16:54:53 +0700

    add server api

--- back-end/server.js
+++ back-end/server.js
@@ -7,12 +7,23 @@ const app=express()
 
 // app.use(express.json())
 
+app.get('/api/products/:id',(req,res)=>{
+    const product =data.products.find((x)=>x._id===req.params.id)
+    if(product){
+        res.send(product)
+    }
+    else{
+        res.status(404)
+           .send({message:'Product not found'})
+    }
+})
+
 app.get('/api/products', (req,res)=>{
     res.send(data.products)
 })
  
 app.get('/', (req,res)=>{
-    res.send("Server is ready  ")
+    res.send("Server is ready ")
 })
 
 app.listen(port,()=>{
